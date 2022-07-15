8aa66286d9919b3e4590d6e4d4d8115748dcfe6b
Matias Sommer <matiassommer1999@gmail.com>
2022-07-15 11:34:02 +0200

    display match

--- pages/display-match.jsx
+++ pages/display-match.jsx
@@ -1,71 +1,146 @@
 import React from "react";
 import { Card, CardActionArea, CardMedia, CardContent, Typography, Button } from "@mui/material";
-import styles from "../styles/DisplayMatch.module.css"
+import styles from '../styles/Network.module.css';
+import { collection, doc, getDoc, getDocs } from 'firebase/firestore';
+import db from '../firebase.config';
+import NetworkCard from "../components/NetworkCard";
+import { useAuth } from "../contexts/Auth";
+import { useRouter } from "next/router";
 
-function DisplayMatch() {
-    const [selectedCard, setSelectedCard] = React.useState(undefined)
-    const cardInfos = [
-        {
-            bild: "bild 1",
-            title: "title 1",
-            content: "body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam.",
-        },
-        {
-            bild: "bild 2",
-            title: "title 2",
-            content: "body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam.",
-        },
-        {
-            bild: "bild 3",
-            title: "title 3",
-            content: "body3. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam.",
-        },
-    ]
+export default function DisplayMatch(props) {
+    const { loggedIn, user } = useAuth();
+    const router = useRouter();
+
     return (
-        <div>
-            <div  className={styles.cardContainer}> 
-                {cardInfos.map((cardInfo, index) => 
-                    <Card className={styles.card}>
-                        <CardActionArea onClick={() => selectedCard === index+1 ? setSelectedCard(undefined) : setSelectedCard(index+1)}>
-                            <CardMedia
-                                component="img"
-                                height="140"
-                                image={cardInfo.bild}
-                                alt="green iguana"
-                            />
-                            <CardContent>
-                                <Typography gutterBottom variant="h5" component="div">
-                                    {cardInfo.title}
-                                </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                    </Card>
-                )}
-            </div>
-            <div className={styles.infoCardContainer}>
-                {selectedCard ? (
-                    <Card className={styles.infoCard}>
-                        <CardContent>
-                            <Typography variant="h5" component="div">
-                                {cardInfos[selectedCard-1].title}
+        <>
+            {loggedIn && (
+                <div>
+                    <div className={styles.container}>
+                        <div className={styles.header}>
+                            <Typography variant="h2">Congratulations!</Typography>
+                            <Typography variant="h4">You were matched with the following buddies</Typography>
+                        </div>
+                        <div className={styles.main}>
+                            {props.network[user.email]?.map((match, index) => (
+                                <NetworkCard
+                                    name={match.name}
+                                    email={match.email}
+                                    skills={match.skills}
+                                    languages={match.languages}
+                                    chatId={match.conversation}
+                                ></NetworkCard>
+                            ))}
+                        </div>
+                    </div>
+                    <div className={styles.buttonContainer}>
+                        <Button className={styles.continueButton} onClick={() => router.push("/network")} variant="contained">
+                            <Typography variant="h5">
+                                Continue
                             </Typography>
-                            <Typography variant="body2">
-                                {cardInfos[selectedCard-1].content}
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                ) : <></> }
-            </div>
-            <div className={styles.buttonContainer}>
-                <Button className={styles.continueButton} variant="contained">
-                    <Typography variant="h3">
-                        Continue
-                    </Typography>
-                </Button>
-            </div>
-          
-        </div>
+                        </Button>
+                    </div>
+                </div>
+            )}
+        </>
     )
 }
 
-export default DisplayMatch;
+const fetchAllSkills = async (email) => {
+    const docRef = doc(db, 'user', email);
+    const user = await getDoc(docRef);
+    const skills = await user.get('skill');
+    const result = [];
+    if (skills) {
+        const skillRef = collection(db, 'skills');
+        const skillCollection = await getDocs(skillRef);
+        await Promise.all(
+            skills.map(async (skill) => {
+                skillCollection.docs.map((sk) => {
+                    const data = sk.data();
+                    console.log(data);
+                    if (skill == data.skillId) result.push(data.skillName);
+                });
+            })
+        );
+    }
+    console.log(result);
+    return result;
+};
+
+const fetchAllLanguages = async (email) => {
+    const docRef = doc(db, 'user', email);
+    const user = await getDoc(docRef);
+    const languages = await user.get('languageId');
+    const result = [];
+    if (languages) {
+        const languagesRef = collection(db, 'languages');
+        const languageCollection = await getDocs(languagesRef);
+        await Promise.all(
+            languages.map(async (language) => {
+                languageCollection.docs.map((l) => {
+                    const data = l.data();
+                    if (language == data.languageId)
+                        result.push(data.languageName);
+                });
+            })
+        );
+    }
+    console.log(result);
+    return result;
+};
+
+const fetchAllMatchInfos = async () => {
+    const matchInfo = {};
+    const conversationsRef = collection(db, 'conversation');
+    const relevantConversations = await getDocs(conversationsRef);
+    await Promise.all(
+        relevantConversations.docs.map(async (conversation) => {
+            // doc.data() is never undefined for query doc snapshots
+            const convData = conversation.data();
+            const docRefBuddy = doc(db, 'user', convData.buddy);
+            const buddyDoc = await getDoc(docRefBuddy);
+            const buddyData = buddyDoc.data();
+
+            const docRefMigrant = doc(db, 'user', convData.migrant);
+            const migrantDoc = await getDoc(docRefMigrant);
+            const migrantData = migrantDoc.data();
+
+            const buddySkills = await fetchAllSkills(buddyData.email);
+            const migrantSkills = await fetchAllSkills(migrantData.email);
+
+            const buddyLanguages = await fetchAllLanguages(buddyData.email);
+            const migrantLanguages = await fetchAllLanguages(migrantData.email);
+
+            if (!matchInfo[migrantData.email])
+                matchInfo[migrantData.email] = [];
+            matchInfo[migrantData.email].push({
+                name: buddyData.name,
+                email: buddyData.email,
+                skills: buddySkills,
+                languages: buddyLanguages,
+                conversation: conversation.id,
+            });
+
+            if (!matchInfo[buddyData.email]) matchInfo[buddyData.email] = [];
+            matchInfo[buddyData.email].push({
+                name: migrantData.name,
+                email: migrantData.email,
+                skills: migrantSkills,
+                languages: migrantLanguages,
+                conversation: conversation.id,
+            });
+        })
+    );
+    return matchInfo;
+};
+
+export async function getStaticProps() {
+    const network = await fetchAllMatchInfos();
+    return {
+        props: {
+            network: JSON.parse(JSON.stringify(network)),
+        },
+    };
+}
+
+
